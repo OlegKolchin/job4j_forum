51d1a7ecd6c489268f0c1da9ade4b6991ddb0173
OlegKolchin <commshepard@gmail.com>
2022-01-15 23:22:21 +0500

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,18 +1,26 @@
 package ru.job4j.forum.control;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -20,6 +28,9 @@ public class PostControlTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private PostService posts;
 
     @Test
     @WithMockUser
@@ -46,9 +57,19 @@ public class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("/post"));
-
     }
 
-
-
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessageWhenSave() throws Exception {
+        this.mockMvc.perform(post("/save")
+                        .param("name", "Куплю ладу-грант. Дорого.")
+                        .param("description", "Описание")
+                        .param("id", "0"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        assertThat(argument.getValue().getName(), is("Куплю ладу-грант. Дорого."));
+    }
 }

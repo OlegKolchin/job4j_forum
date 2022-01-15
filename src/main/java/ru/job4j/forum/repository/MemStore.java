09e65f3521311e94c7dce1986a4ac73e4d0ce7b6
OlegKolchin <commshepard@gmail.com>
2022-01-15 22:37:23 +0500

    0. Spring Boot [#6880 #250878]

--- src/main/java/ru/job4j/forum/repository/MemStore.java
+++ src/main/java/ru/job4j/forum/repository/MemStore.java
@@ -2,40 +2,40 @@ package ru.job4j.forum.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.job4j.forum.model.Post;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
 
 @Repository
 public class MemStore {
-    private List<Post> posts = new ArrayList<>();
+    private AtomicInteger id = new AtomicInteger(0);
+    private HashMap<Integer, Post> posts = new HashMap<>();
 
     public MemStore() {
         Post post = Post.of("Продаю машину ладу 01.");
-        post.setId(1);
-        posts.add(post);
+        post.setId(id.incrementAndGet());
+        posts.put(id.get(), post);
     }
 
     public Post save(Post post) {
-        for (Post p : posts) {
-            if (p.getId() == post.getId()) {
-                p.setName(post.getName());
-                p.setCreated(post.getCreated());
-                p.setDescription(post.getDescription());
-                return post;
-            }
+        if (!posts.containsKey(post.getId())) {
+            post.setId(id.incrementAndGet());
+            posts.put(id.get(), post);
+        } else {
+            posts.replace(post.getId(), post);
         }
-        post.setId(posts.size() + 1);
-        posts.add(post);
         return post;
+
     }
 
     public List<Post> getPosts() {
-        return posts;
+        return new ArrayList<>(posts.values());
     }
 
     public Post findById(int id) {
-        return posts.stream().filter(p -> p.getId() == id)
-                .findFirst().get();
+        return posts.get(id);
     }
 }
